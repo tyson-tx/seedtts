26bb743ef464389d2869737103e65b2a90176dec
tyson-tx <tysontx@163.com>
2024-06-06 22:05:41 +0800

    修改Hero页面 && 多语言修改

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = ({ locale, CTALocale }: { locale: any; CTALocale: any }) => {
       >
         <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-16 md:pt-24 text-center">
           <h1>
-            {locale.title1} <LineText>{locale.title2}</LineText> {locale.title3}
+          <LineText>{locale.title1}</LineText> {locale.title2} {locale.title3}
           </h1>
           <p className="mx-auto mt-6 max-w-2xl text-2xl tracking-tight text-slate-700 dark:text-slate-400">
             {/* {siteConfig.description} */}
